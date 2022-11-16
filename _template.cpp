e54ec5bc86c27f18ff0e38dc6872b1e4addedfc7
Łukasz Skabowski <lukasz1908@gmail.com>
2022-11-16 14:07:35 +0100

    Added a template for future programs

---
+++ _template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
